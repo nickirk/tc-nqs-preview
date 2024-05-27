7473c14c2f879781afde96884eed2ad5106d4d77
Ke Liao <ke.liao.whu@gmail.com>
2024-05-27 14:44:50 +0200

    rename folder

--- tcnqs/backflow.py
+++ tcnqs/backflow.py
@@ -5,19 +5,17 @@ import numpy as np
 
 
 class BACKFLOW(nn.Module):
-    # number of electrons
-    n_e: int
-    # number of total orbitals 
-    n_orb: int
-    # number of slater determinants
-    n_sd: int = 1
+    features: Sequence[int] 
+
+    def setup(self):
+        self.layers = [nn.Dense(n) for n in self.features]
 
     @nn.compact
-    def __call__(self, x, params):
-        if n_e > n_orb:
-            raise ValueError("Number of electrons should be less than or equal to number of orbitals")
-        x = nn.Dense(features=2*n_orb)(x)
-        x = nn.Dense(features=10)(x)
-        x = nn.relu(x)
-        x = nn.Dense(features=(n_orb, n_orb))(x)
+    def __call__(self, x):
+        for i, lyr in enumerate(self.layers):
+          x = lyr(x)
+          if i != len(self.layers) - 1:
+            x = nn.relu(x) 
+        # TODO: select corresponding rows and columns of the matrix
+        x = jnp.linalg.det(x)
         return x
