18f0b2127b5d44729ffc333249064c45824210ef
Unik-W <unikwadhwani@gmail.com>
2024-07-12 14:40:12 +0200

    completely working

--- tcnqs/hamiltonian_jit.py
+++ tcnqs/hamiltonian_jit.py
@@ -1,27 +1,25 @@
 import jax.numpy as jnp
 from jax import jit, lax
+import jax
 
-# Debug in progress 
+# Potential Issue: not compitable with different number of electrons in alpha and beta seprated orbitals
 
 class HAMILTONIAN:
-
+    # n_orb is the number of orbitals in the alpha and beta seprated orbitals
     def __init__(self, n_elec, n_orb, h1g, g2e):
         self.n_elec = n_elec
         self.n_orb = n_orb 
         self.h1g = h1g
         self.g2e = g2e
     
+    # By convention det1 is the bra and det2 is the ket always
     def __call__(self, det1, det2):
         return self._get_1body(det1, det2)  + self._get_2body(det1, det2)
     
-    ## NOT VERY EFFICIENT IMPLEMENTATION ##
-    ## CHANGE ##
+    # Potential Issue: Only for even number of electrons in the alpha and beta seprated orbitals both
     def phase(self,det,j):
-        sum_result = jnp.sum(jnp.where(jnp.arange(len(det)) < j, det, 0))
-        print(1 - 2 * (sum_result % 2))
+        sum_result = jnp.sum(jnp.where(jnp.arange(self.n_orb) < j, det, 0))
         return 1 - 2 * (sum_result % 2)
-
-    
     
     def _get_1body(self, det1, det2):
         diff = jnp.bitwise_xor(det1, det2)
@@ -39,7 +37,7 @@ class HAMILTONIAN:
             return (phase_global * self.h1g[i,j])
         
 
-        return lax.cond(num_diff == 2, 
+        return jax.lax.cond(num_diff == 2, 
                                 diff_2,
                         lambda : lax.cond(
                         num_diff == 0, diff_0, lambda : 0.0))
@@ -61,7 +59,10 @@ class HAMILTONIAN:
             j = diff_index[jnp.where(det2[diff_index]==1, size=1)][0]
             
             sum_indices = jnp.where(jnp.logical_and(det1, det2),size=self.n_elec-1)[0]
-            return jnp.sum(self.g2e[k, sum_indices, sum_indices, j] - self.g2e[k, sum_indices, j, sum_indices])
+            
+            phase_global=self.phase(det1,k) * self.phase(det2,j)
+            
+            return phase_global*jnp.sum(self.g2e[k, sum_indices, sum_indices, j] - self.g2e[k, sum_indices, j, sum_indices])
 
         def diff_4():
             diff_index = jnp.nonzero(diff,size=4)[0]
@@ -74,10 +75,12 @@ class HAMILTONIAN:
             
             phase_global = self.phase(det1,i)*self.phase(det1,k)*self.phase(det2,j)*self.phase(det2,l)
             
+            #print(phase_global==1)
             
             return phase_global*(self.g2e[i, k, l, j] - self.g2e[i, k, j, l])
 
-        return lax.cond(num_diff == 4, 
+        return jax.lax.cond(num_diff == 4, 
                         diff_4,
-                        lambda : lax.cond(num_diff == 2, diff_2, 
-                                           lambda : lax.cond(num_diff == 0, diff_0, lambda : 0.0)))
+                        lambda : jax.lax.cond(num_diff == 2, diff_2, 
+                                           lambda : jax.lax.cond(num_diff == 0, diff_0, lambda : 0.0)))
+
